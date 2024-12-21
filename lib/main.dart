8b39e4b399f0e6eb2f085a66e5cb825e36acf0af
Rusiru_Chapana <rusiruchapana@gmail.com>
2024-12-21 04:26:04 +0000

    #

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_list_/screens/todo_screen.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const ToDoListApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class ToDoListApp extends StatelessWidget {
+  const ToDoListApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      title: "To Do List App",
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const ToDoScreen(),
     );
   }
 }
---
+++ lib/screens/todo_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ToDoScreen extends StatefulWidget {
+  const ToDoScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ToDoScreen> createState() => _ToDoScreenState();
+}
+
+class _ToDoScreenState extends State<ToDoScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("To Do List App")),
+    );
+  }
+}
